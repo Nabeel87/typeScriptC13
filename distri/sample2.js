52da32e0a333cb0929aad2321e9603f29cc92974
Nabell87 <nabilsabir786@gmail.com>
2023-09-28 19:31:35 +0500

    data type :any
    bad approach dont use this

--- distri/sample2.js
+++ distri/sample2.js
@@ -1,7 +1,12 @@
 "use strict";
-var isActive = true;
+// const isActive: boolean = true;
 document.write("Hello TypeScript");
-function isEven(num) {
-    return num % 2 === 0;
-}
-console.log(isEven(5));
+// function isEven(num: number){
+//     return num % 2 ===0;
+// }
+// // console.log(isEven(10));
+// console.log(isEven("10"));
+var a = 1;
+a = true;
+a = [1, 2, 3];
+console.log(a);
--- src/sample2.ts
+++ src/sample2.ts
@@ -1,9 +1,19 @@
-const isActive: boolean = true;
+// const isActive: boolean = true;
 
 document.write("Hello TypeScript")
 
-function isEven(num: number){
-    return num % 2 ===0;
-}
+// function isEven(num: number){
+//     return num % 2 ===0;
+// }
 
-console.log(isEven(5));
+// // console.log(isEven(10));
+
+
+// console.log(isEven("10"));
+
+
+let a: any = 1;
+a = true;
+a = [1,2,3];
+
+console.log(a);
