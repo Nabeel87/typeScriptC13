1a895ca625afd24f1363942aeeca1bf5217a242d
Nabell87 <nabilsabir786@gmail.com>
2023-09-28 12:34:58 +0500

    practice

--- src/sample.js
+++ src/sample.js
@@ -1,1 +1,14 @@
-console.log("hello World from TS");
+// console.log("hello World from TS");
+// let a  = 1 ; 
+// a = true;
+// function a(b: number) {
+// return b / 2
+// }
+// a("55")
+var firstName = "Ali";
+var lastName = "Hassan";
+var age = 35;
+var isActiveMember = true;
+var cources = ["a", "b", "c", "d"];
+// let fullName: number = firstName + age;
+var fullName = firstName + lastName + age;
--- src/sample.ts
+++ src/sample.ts
@@ -1,1 +1,23 @@
-console.log("hello World from TS");
+// console.log("hello World from TS");
+
+
+// let a  = 1 ; 
+// a = true;
+
+
+
+// function a(b: number) {
+// return b / 2
+// }
+// a("55")
+
+
+let firstName: string = "Ali";
+let lastName: string = "Hassan";
+let age: number = 35;
+let isActiveMember: boolean = true;
+let cources: string[] = ["a", "b", "c", "d"];
+
+// let fullName: number = firstName + age;
+
+let fullName: string = firstName + lastName + age  ;
