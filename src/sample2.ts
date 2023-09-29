ec446ec2921ab6e7422465f20a5ad5828e26a945
Nabell87 <nabilsabir786@gmail.com>
2023-09-29 18:51:49 +0500

    Update sample2.ts

--- src/sample2.ts
+++ src/sample2.ts
@@ -12,8 +12,17 @@ document.write("Hello TypeScript")
 // console.log(isEven("10"));
 
 
-let a: any = 1;
-a = true;
-a = [1,2,3];
+// let a: any = 1;
+// a = true;
+// a = [1,2,3];
+
+// console.log(a);
+
+let a: unknown = 30;
+let b = a === 123;
+// let c = a + 10;
+if (typeof a === 'number'){
+    let d = a + 10
+}
 
 console.log(a);
